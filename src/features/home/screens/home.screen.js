813639f5a47fe90dced38c1330a622d3e3d5a727
procode47 <iloenyenwavictor@gmail.com>
2022-09-13 22:14:22 +0100

    feat: connected to spoonacular API

--- src/features/home/screens/home.screen.js
+++ src/features/home/screens/home.screen.js
@@ -13,10 +13,35 @@ import {
   BackDrop,
 } from "../components/home.styles";
 import { Avatar } from "react-native-paper";
-import { ScrollView, View, ImageBackground } from "react-native";
+import { ScrollView, View, ImageBackground, FlatList } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 export const HomeScreen = ({ navigation }) => {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    // const check = localStorage.getItem("popular");
+
+    if (false) {
+      setPopular(JSON.parse(check));
+    } else {
+      try {
+        const api = await fetch(
+          `https://api.spoonacular.com/recipes/random?apiKey=97a3d35ad8ce4f46a4fcdbdd6d25e69a&number=10`
+        );
+        const data = await api.json();
+        console.log(data.recipes);
+        // localStorage.setItem("popular", JSON.stringify(data.recipes));
+        setPopular(data.recipes);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
   return (
     <SafeArea style={{ backgroundColor: "white" }}>
       <HeaderRow>
@@ -47,7 +72,7 @@ export const HomeScreen = ({ navigation }) => {
         </MealTagScroll>
       </View>
       <Spacer position="top" size="small" />
-      <ScrollView>
+      {/* <ScrollView>
         <RecipeItem>
           <ImageBackground
             style={{ width: "100%", height: "100%" }}
@@ -157,7 +182,42 @@ export const HomeScreen = ({ navigation }) => {
           </ImageBackground>
         </RecipeItem>
 
-      </ScrollView>
+      </ScrollView> */}
+      <FlatList
+        data={popular}
+        renderItem={({ item }) => {
+          return (
+            <RecipeItem>
+              <ImageBackground
+                style={{ width: "100%", height: "100%" }}
+                source={{ uri: item.image }}
+              >
+                <LinearGradient
+                  colors={["#00000000", "#000000"]}
+                  style={{ height: "100%", width: "100%" }}
+                >
+                  <View style={{ position: "absolute", top: 25, right: 15 }}>
+                    <Text variant="card_rating">
+                      <Feather name="star" size={26} color={"#FFF"} /> 4.5
+                    </Text>
+                  </View>
+                  <View style={{ position: "absolute", bottom: 15, left: 15 }}>
+                    <Text variant="card_heading">{item.title}</Text>
+                    <Spacer position="top" size="medium" />
+                    <Row>
+                      <Feather name="clock" size={18} color={"#FFF"} />
+                      <Spacer position="left" size="medium">
+                        <Text variant="card_timer">15 min</Text>
+                      </Spacer>
+                    </Row>
+                  </View>
+                </LinearGradient>
+              </ImageBackground>
+            </RecipeItem>
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      />
     </SafeArea>
   );
 };
